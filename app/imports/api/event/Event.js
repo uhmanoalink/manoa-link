7448de8b234630910eb16a2d6e4d51a9e9e8ebff
gitCarrot <jeonhg0@gmail.com>
2023-04-07 06:44:02 +0900

    Add API for components

---
+++ app/imports/api/event/Event.js
@@ -0,0 +1,35 @@
+import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+
+/**
+ * The EventsCollection. It encapsulates state and variable values for stuff.
+ */
+class EventsCollection {
+  constructor() {
+    // The name of this collection.
+    this.name = 'EventsCollection';
+    // Define the Mongo collection.
+    this.collection = new Mongo.Collection(this.name);
+    // Define the structure of each document in the collection.
+    this.schema = new SimpleSchema({
+      eventName: String,
+      address: String,
+      description: String,
+      companyId: String,
+      createdAt: Date,
+      owner: String,
+    });
+    // Attach the schema to the collection, so all attempts to insert a document are checked against schema.
+    this.collection.attachSchema(this.schema);
+    // Define names for publications and subscriptions
+    this.userPublicationName = `${this.name}.publication.user`;
+    this.companyPublicationName = `${this.name}.publication.user`;
+    this.adminPublicationName = `${this.name}.publication.admin`;
+  }
+}
+
+/**
+ * The singleton instance of the EventsCollection.
+ * @type {EventsCollection}
+ */
+export const Events = new EventsCollection();
