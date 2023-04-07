d6e97d993eb86e782bd7714c381da47712e8560c
jlo2224 <118222769+jlo2224@users.noreply.github.com>
2023-04-07 07:41:20 -1000

    Revert "Add API for components"

--- app/imports/api/stuff/Stuff.js
+++ app/imports/api/stuff/Stuff.js
@@ -2,33 +2,35 @@ import { Mongo } from 'meteor/mongo';
 import SimpleSchema from 'simpl-schema';
 
 /**
- * The UsersCollection. It encapsulates state and variable values for stuff.
+ * The StuffsCollection. It encapsulates state and variable values for stuff.
  */
-class UsersCollection {
+class StuffsCollection {
   constructor() {
     // The name of this collection.
-    this.name = 'UsersCollection';
+    this.name = 'StuffsCollection';
     // Define the Mongo collection.
     this.collection = new Mongo.Collection(this.name);
     // Define the structure of each document in the collection.
     this.schema = new SimpleSchema({
-      firstName: String,
-      lastName: String,
-      address: String,
-      major: String,
+      name: String,
+      quantity: Number,
       owner: String,
+      condition: {
+        type: String,
+        allowedValues: ['excellent', 'good', 'fair', 'poor'],
+        defaultValue: 'good',
+      },
     });
     // Attach the schema to the collection, so all attempts to insert a document are checked against schema.
     this.collection.attachSchema(this.schema);
     // Define names for publications and subscriptions
     this.userPublicationName = `${this.name}.publication.user`;
-    this.companyPublicationName = `${this.name}.publication.user`;
     this.adminPublicationName = `${this.name}.publication.admin`;
   }
 }
 
 /**
- * The singleton instance of the UsersCollection.
- * @type {UsersCollection}
+ * The singleton instance of the StuffsCollection.
+ * @type {StuffsCollection}
  */
-export const Users = new UsersCollection();
+export const Stuffs = new StuffsCollection();
