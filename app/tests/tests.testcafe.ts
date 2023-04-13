9e686b686f10d64588c244d3f79e7627ecc0d5b2
Winston Co <56942091+winstonco@users.noreply.github.com>
2023-04-13 09:51:34 -1000

    56 update footer (#57)
    * updated footer component, added new fonts
    
    * add basic tests for footer component
    
    * Add failing test to ensure workflow is working properly
    
    * Remove failing test
    
    * ignore css and css map files created

--- app/tests/tests.testcafe.ts
+++ app/tests/tests.testcafe.ts
@@ -1,30 +1,16 @@
 import {} from 'testcafe';
 
-import { landingPage } from './landing.page';
 import Credentials from './types/CredentialsType';
-
-// import { signinPage } from './signin.page';
-// import { signoutPage } from './signout.page';
+import { landingPage } from './landing.page';
 import { navBar } from './navbar.component';
 import { dashboardPage } from './dashboard.page';
-
-/* global fixture:false, test:false */
+import { footerComponent } from './footer.component';
 
 /** Credentials for one of the sample users defined in settings.development.json. */
 const userCredentials: Credentials = { username: 'john@foo.com', password: 'changeme' };
 const companyCredentials: Credentials = { username: 'company@foo.com', password: 'changeme' };
 
-fixture('ManoaLink localhost test with default db').page(
-  'http://localhost:3000',
-);
-
-/*
-  Test format:
-  test(testName, async (t) => {
-    await test1;
-    await test2;
-  });
-*/
+fixture('ManoaLink localhost test with default db').page('http://localhost:3000');
 
 test('Test landing page', async (testController) => {
   await landingPage.test(testController, userCredentials);
@@ -40,10 +26,6 @@ test('Test dashboard page', async (tc) => {
   await dashboardPage.test(tc);
 });
 
-// test('Test that signin and signout work', async (testController) => {
-//   await navBar.gotoSignInPage(testController);
-//   await signinPage.signin(testController, credentials.username, credentials.password);
-//   await navBar.isLoggedIn(testController, credentials.username);
-//   await navBar.logout(testController);
-//   await signoutPage.isDisplayed(testController);
-// });
+test('Test footer component', async (testController) => {
+  await footerComponent.test(testController);
+});
