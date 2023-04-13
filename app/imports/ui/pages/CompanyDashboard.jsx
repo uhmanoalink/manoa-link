7d6597015094599f0d2fcd72298dfc6edcc370cf
Winston Co <winstonco@live.com>
2023-04-13 02:19:26 -1000

    create initial company dashboard
    shows: some upcoming events, some listings, a calendar widget

---
+++ app/imports/ui/pages/CompanyDashboard.jsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import { Meteor } from 'meteor/meteor';
+import { Col, Container, Row, Badge } from 'react-bootstrap';
+import { useTracker } from 'meteor/react-meteor-data';
+import { Stuffs } from '../../api/stuff/Stuff';
+import Sidebar from '../components/Sidebar';
+import LoadingSpinner from '../components/LoadingSpinner';
+
+/* Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
+const CompanyDashboard = () => {
+
+  const sampleEvents = [
+    {
+      name: 'This is a sample event',
+      image: '/images/sample-image-landscape.png',
+      date: 'April 13, 2023',
+    },
+  ];
+
+  return (
+    <Container id="dashboard" className="py-3">
+      <Row className="justify-content-center">
+        <Col xs={3}>
+          <Sidebar />
+        </Col>
+        <Col>
+          <main>
+            <section id="upcoming-events">
+              <h2>Upcoming Events</h2>
+              <div className="events">
+                <div className="event">
+                  <h3 className="event-name">This is a sample event</h3>
+                  <img src="/images/sample-image-landscape.png" alt="sample" />
+                  <h4 className="event-date">April 13, 2023</h4>
+                </div>
+                <div className="event">
+                  <h3 className="event-name">This is another sample event</h3>
+                  <img src="/images/sample-image-landscape.png" alt="sample" />
+                  <h4 className="event-date">April 15, 2023</h4>
+                </div>
+                <div className="event">
+                  <h3 className="event-name">This is one final sample event</h3>
+                  <img src="/images/sample-image-landscape.png" alt="sample" />
+                  <h4 className="event-date">April 17, 2023</h4>
+                </div>
+              </div>
+            </section>
+            <section id="listings">
+              <h2>Listings</h2>
+              <hr />
+              <div className="listing-group">
+                <h3>Software engineering</h3>
+                <hr />
+                <div className="listing">
+                  <a href="/" className="listing-name">
+                    Summer Web Developer Internship
+                  </a>
+                  <Badge>Num. Applicants: 400</Badge>
+                </div>
+                <div className="listing">
+                  <a href="/" className="listing-name">
+                    Summer DevOps Internship
+                  </a>
+                  <Badge>Num. Applicants: 300</Badge>
+                </div>
+              </div>
+              <div className="listing-group">
+                <h3>Human Resources</h3>
+                <hr />
+                <div className="listing">
+                  <a href="/" className="listing-name">
+                    Summer HR Internship
+                  </a>
+                  <Badge>Num. Applicants: 100</Badge>
+                </div>
+              </div>
+            </section>
+            <section id="calendar">
+              <h2>A calendar widget</h2>
+            </section>
+          </main>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default CompanyDashboard;
