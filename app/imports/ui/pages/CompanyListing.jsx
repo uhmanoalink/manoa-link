ca2ee4fc072ac4fe11511e5328530d602730968f
jlo2224 <jaredlo@hawaii.edu>
2023-04-13 08:01:21 -1000

    Made Company Listing Mockup

--- app/imports/ui/pages/CompanyListing.jsx
+++ app/imports/ui/pages/CompanyListing.jsx
@@ -1,8 +1,87 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Button, Card, Container, Col, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import Sidebar from '../components/Sidebar';
 
-const CompanyListing = () => (
-  <Container className="py-3" />
-);
+const CompanyListing = () => {
+
+  const sampleCompanies = [
+    {
+      image: 'images/sample-image-landscape.png',
+      name: 'RevaComm',
+      text: 'A mission-driven digital transformation firm focused on Agile DevSecOps practices that deliver scale, speed, and security for government agencies and enterprises facing continually evolving IT needs and cyber security threats.',
+      link: '#'
+    },
+    {
+      image: 'images/sample-image-landscape.png',
+      name: 'NASA',
+      text: 'NASA explores the unknown in air and space, innovates for the benefit of humanity, and inspires the world through discovery.',
+      link: '#'
+    },
+    {
+      image: 'images/sample-image-landscape.png',
+      name: 'ISEC',
+      text: 'The mission of ISEC Conferences is to enhance communication and understanding between structural, system, and construction engineers, for successful design and construction of engineering projects. Interdisciplinary integration and international cooperation are encouraged.',
+      link: '#'
+    },
+    {
+      image: 'images/sample-image-landscape.png',
+      name: 'OpenAI',
+      text: 'ChatGPT is a state-of-the-art language model developed by OpenAI, capable of providing personalized and intelligent responses to a wide range of queries and conversations.',
+      link: '#'
+    },
+    {
+      image: 'images/sample-image-landscape.png',
+      name: 'Sample',
+      text: 'Sample-text',
+      link: '#'
+    },
+    {
+      image: 'images/sample-image-landscape.png',
+      name: 'Sample-2',
+      text: 'Sample-text-2',
+      link: '#'
+    }
+  ]
+
+  return (
+    <Container id="company-listing" className="py-3">
+      <Row className="justify-content-center">
+        <Col xs={12} md={3}>
+          <Sidebar />
+        </Col>
+        { sampleCompanies.map(( { image, name, text, link }, index ) => {
+          if (index % 3 == 0 && index != 0) {
+            return (
+              <>
+                <Col xs={12} md={3}>
+                </Col>
+                <Col xs={12} md={3}>
+                  <Card id="company-card">
+                    <Card.Img id="company-card-image" variant="top" src={image}/>
+                    <Card.Title id="company-card-title">{name}</Card.Title>
+                    <Card.Text id="company-card-text">{text}</Card.Text>
+                    <Card.Link id="company-card-link" to={link}><Button>Visit</Button></Card.Link>
+                  </Card>
+                </Col>
+              </>
+            )
+          } else {
+            return (
+              <Col xs={12} md={3}>
+                <Card id="company-card">
+                  <Card.Img id="company-card-image" variant="top" src={image}/>
+                  <Card.Title id="company-card-title">{name}</Card.Title>
+                  <Card.Text id="company-card-text">{text}</Card.Text>
+                  <Card.Link id="company-card-link" to={link}><Button>Visit</Button></Card.Link>
+                </Card>
+              </Col>
+            )
+          }
+        }) }
+      </Row>
+    </Container>
+  );
+}
 
 export default CompanyListing;
