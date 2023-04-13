26bcd017df911a287bf2a5e753c4f62e6f652f44
jlo2224 <jaredlo@hawaii.edu>
2023-04-13 08:07:28 -1000

    Fixed lint errors

--- app/imports/ui/pages/CompanyListing.jsx
+++ app/imports/ui/pages/CompanyListing.jsx
@@ -10,39 +10,39 @@ const CompanyListing = () => {
       image: 'images/sample-image-landscape.png',
       name: 'RevaComm',
       text: 'A mission-driven digital transformation firm focused on Agile DevSecOps practices that deliver scale, speed, and security for government agencies and enterprises facing continually evolving IT needs and cyber security threats.',
-      link: '#'
+      link: '#',
     },
     {
       image: 'images/sample-image-landscape.png',
       name: 'NASA',
       text: 'NASA explores the unknown in air and space, innovates for the benefit of humanity, and inspires the world through discovery.',
-      link: '#'
+      link: '#',
     },
     {
       image: 'images/sample-image-landscape.png',
       name: 'ISEC',
-      text: 'The mission of ISEC Conferences is to enhance communication and understanding between structural, system, and construction engineers, for successful design and construction of engineering projects. Interdisciplinary integration and international cooperation are encouraged.',
-      link: '#'
+      text: 'The mission of ISEC Conferences is to enhance communication and understanding between structural, system, and construction engineers, for successful design and construction of engineering projects.',
+      link: '#',
     },
     {
       image: 'images/sample-image-landscape.png',
       name: 'OpenAI',
       text: 'ChatGPT is a state-of-the-art language model developed by OpenAI, capable of providing personalized and intelligent responses to a wide range of queries and conversations.',
-      link: '#'
+      link: '#',
     },
     {
       image: 'images/sample-image-landscape.png',
       name: 'Sample',
       text: 'Sample-text',
-      link: '#'
+      link: '#',
     },
     {
       image: 'images/sample-image-landscape.png',
       name: 'Sample-2',
       text: 'Sample-text-2',
-      link: '#'
-    }
-  ]
+      link: '#',
+    },
+  ];
 
   return (
     <Container id="company-listing" className="py-3">
@@ -50,38 +50,36 @@ const CompanyListing = () => {
         <Col xs={12} md={3}>
           <Sidebar />
         </Col>
-        { sampleCompanies.map(( { image, name, text, link }, index ) => {
-          if (index % 3 == 0 && index != 0) {
+        { sampleCompanies.map(({ image, name, text, link }, index) => {
+          if (index % 3 === 0 && index !== 0) {
             return (
               <>
-                <Col xs={12} md={3}>
-                </Col>
+                <Col xs={12} md={3} />
                 <Col xs={12} md={3}>
                   <Card id="company-card">
-                    <Card.Img id="company-card-image" variant="top" src={image}/>
+                    <Card.Img id="company-card-image" variant="top" src={image} />
                     <Card.Title id="company-card-title">{name}</Card.Title>
                     <Card.Text id="company-card-text">{text}</Card.Text>
                     <Card.Link id="company-card-link" to={link}><Button>Visit</Button></Card.Link>
                   </Card>
                 </Col>
               </>
-            )
-          } else {
-            return (
-              <Col xs={12} md={3}>
-                <Card id="company-card">
-                  <Card.Img id="company-card-image" variant="top" src={image}/>
-                  <Card.Title id="company-card-title">{name}</Card.Title>
-                  <Card.Text id="company-card-text">{text}</Card.Text>
-                  <Card.Link id="company-card-link" to={link}><Button>Visit</Button></Card.Link>
-                </Card>
-              </Col>
-            )
+            );
           }
+          return (
+            <Col xs={12} md={3}>
+              <Card id="company-card">
+                <Card.Img id="company-card-image" variant="top" src={image} />
+                <Card.Title id="company-card-title">{name}</Card.Title>
+                <Card.Text id="company-card-text">{text}</Card.Text>
+                <Card.Link id="company-card-link" to={link}><Button>Visit</Button></Card.Link>
+              </Card>
+            </Col>
+          );
         }) }
       </Row>
     </Container>
   );
-}
+};
 
 export default CompanyListing;
