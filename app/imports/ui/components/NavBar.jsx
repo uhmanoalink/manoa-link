5a363a2c13486608b82d4d092fa18e02ab45ed73
Winston Co <winstonco@live.com>
2023-04-15 11:22:17 -1000

    add hamburger menu, ProtectedRender, navbar styles

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { useTracker } from 'meteor/react-meteor-data';
 import { NavLink, useLocation } from 'react-router-dom';
-import { Roles } from 'meteor/alanning:roles';
-import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Container, Image, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import { BoxArrowRight, PersonFill, PersonPlusFill } from 'react-bootstrap-icons';
+
+import ProtectedRender from './ProtectedRender';
 
 const NavBar = () => {
   // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
@@ -17,60 +18,112 @@ const NavBar = () => {
 
   const location = useLocation();
 
-  return location.pathname === '/' ? undefined : (
-    <Navbar bg="light" expand="lg">
+  return location.pathname === '/' ? null : (
+    <Navbar expand="md" id="navbar">
       <Container>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Brand href="/">
+          Manoa Link
+          {/* <Image src="/images/logo.png" alt="Logo" width={85} /> */}
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav">
+          <svg className="hamburger" width={35} height={30} viewBox="0 0 35 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect className="hamburger-1" x={0} y={0} width={35} height={4} fill="white" rx={2} />
+            <rect className="hamburger-2" x={0} y={13} width={35} height={4} fill="white" rx={2} />
+            <rect className="hamburger-3" x={0} y={26} width={35} height={4} fill="white" rx={2} />
+          </svg>
+        </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto justify-content-start">
-            {(Roles.userIsInRole(Meteor.userId(), 'student') || Roles.userIsInRole(Meteor.userId(), 'admin')) ? (
-              <Nav.Link id="my-profile-nav" as={NavLink} to="/my-profile" key="my-profile">
-                My Profile
+          <Nav className="ms-auto">
+            <ProtectedRender>
+              <Nav.Link
+                as={NavLink}
+                to="/dashboard"
+              >
+                Dashboard
               </Nav.Link>
-            ) : undefined}
-            {currentUser ? (
-              <>
-                <Nav.Link id="dashboard-nav" as={NavLink} to="/dashboard" key="dashboard">
-                  Dashboard
+              <ProtectedRender allowedRoles={['company']}>
+                <Nav.Link
+                  as={NavLink}
+                  to="/company-listing"
+                >
+                  Manage Listings
                 </Nav.Link>
-                <Nav.Link id="add-events-nav" as={NavLink} to="/add-events" key="add-events">
-                  Add Events
+                <Nav.Link
+                  as={NavLink}
+                  to="/manage-events"
+                >
+                  Manage Events
                 </Nav.Link>
-                <Nav.Link id="list-events-nav" as={NavLink} to="/list-events" key="list-events">
-                  List Events
+              </ProtectedRender>
+              <ProtectedRender allowedRoles={['student']}>
+                <Nav.Link
+                  as={NavLink}
+                  to="/company-listing"
+                >
+                  Job Listings
                 </Nav.Link>
-                <Nav.Link id="company-listing-nav" as={NavLink} to="/company-listing" key="company-listing">
-                  Company Listing
+                <Nav.Link
+                  as={NavLink}
+                  to="/events"
+                >
+                  Events Board
                 </Nav.Link>
-              </>
-            ) : undefined}
-            {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
-              <Nav.Link id="list-stuff-admin-nav" as={NavLink} to="/admin" key="admin">
-                Admin
-              </Nav.Link>
-            ) : undefined}
-          </Nav>
-          <Nav className="justify-content-end">
-            {currentUser === '' ? (
-              <NavDropdown id="login-dropdown" title="Login">
-                <NavDropdown.Item id="login-dropdown-sign-in" as={NavLink} to="/signin">
+                <ProtectedRender allowedRoles={['admin']}>
+                  <Nav.Link
+                    as={NavLink}
+                    to="/"
+                  >
+                    TO DO
+                  </Nav.Link>
+                </ProtectedRender>
+              </ProtectedRender>
+
+              <ProtectedRender allowedRoles={['admin']}>
+                <Nav.Link
+                  as={NavLink}
+                  to="/admin"
+                >
+                  Admin
+                </Nav.Link>
+              </ProtectedRender>
+            </ProtectedRender>
+            {(currentUser === '') ? (
+              <NavDropdown align="end" title="Login">
+                <NavDropdown.Item as={NavLink} to="/signin">
                   <PersonFill />
                   Sign in
                 </NavDropdown.Item>
-                <NavDropdown.Item id="login-dropdown-sign-up" as={NavLink} to="/signup">
+                <NavDropdown.Item as={NavLink} to="/signup">
                   <PersonPlusFill />
                   Sign up
                 </NavDropdown.Item>
               </NavDropdown>
             ) : (
-              <NavDropdown id="navbar-current-user" title={currentUser}>
-                <NavDropdown.Item id="navbar-sign-out" as={NavLink} to="/signout">
+              <NavDropdown
+                align="end"
+                title={(
+                  <Image
+                    src="/images/sample-pfp.png"
+                    alt="pfp"
+                    width={36}
+                  />
+                )}
+              >
+                <NavDropdown.Item
+                  id="my-profile-nav"
+                  as={NavLink}
+                  to="/my-profile"
+                >
+                  Profile
+                </NavDropdown.Item>
+                <NavDropdown.Item as={NavLink} to="/signout">
                   <BoxArrowRight /> Sign out
                 </NavDropdown.Item>
               </NavDropdown>
             )}
           </Nav>
         </Navbar.Collapse>
+
       </Container>
     </Navbar>
   );
