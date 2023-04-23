d70cebcbbeee2b7bf456f758f0eb0c5c49df4ae1
jlo2224 <jaredlo@hawaii.edu>
2023-04-22 14:57:09 -1000

    Fixed lint errors

--- app/imports/ui/components/Sidebar.jsx
+++ app/imports/ui/components/Sidebar.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import LoadingSpinner from './LoadingSpinner';
 import PropTypes from 'prop-types';
+import LoadingSpinner from './LoadingSpinner';
 import Landing from '../pages/Landing';
 
-const Sidebar = ({ children }) => {
-  return (
-    <div id="sidebar">
-      {children}
-    </div>
-  );
-};
+const Sidebar = ({ children }) => (
+  <div id="sidebar">
+    {children}
+  </div>
+);
 
 // Require a component and location to be passed to each AdminProtectedRoute.
 Sidebar.propTypes = {
