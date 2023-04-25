b81e236287afcf6ecd876de6bf36ccded6993659
Jared Lo <118222769+jlo2224@users.noreply.github.com>
2023-04-24 21:39:42 -1000

    Update Sidebar.jsx

--- app/imports/ui/components/Sidebar.jsx
+++ app/imports/ui/components/Sidebar.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import LoadingSpinner from './LoadingSpinner';
-import Landing from '../pages/Landing';
 
 const Sidebar = ({ children }) => (
   <div id="sidebar">
