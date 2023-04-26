26a68424ff68b9d81bde9e4995693a333edd0852
Winston Co <winstonco@live.com>
2023-04-25 20:33:35 -1000

    add calendar and use database

--- app/imports/ui/pages/CompanyDashboard.jsx
+++ app/imports/ui/pages/CompanyDashboard.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { useTracker } from 'meteor/react-meteor-data';
-import { Col, Container, Row, Badge } from 'react-bootstrap';
-// import Sidebar from '../components/Sidebar';
+import { Badge } from 'react-bootstrap';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
 import HelpButton from '../components/HelpButton';
 import { Events } from '../../api/event/Event';
+import Event from '../components/Event';
 import { Positions } from '../../api/position/Position';
+import Position from '../components/Position';
+import { Companies } from '../../api/company/Company';
+import Company from '../components/Company';
 
 /* Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
 const CompanyDashboard = () => {
 
-  const { events, listings } = useTracker(() => {
+  const { events, listings, company } = useTracker(() => {
     const eventsSub = Meteor.subscribe(Events.companyPublicationName);
     const listingsSub = Meteor.subscribe(Positions.companyPublicationName);
     // Determine if the subscription is ready
@@ -18,90 +23,88 @@ const CompanyDashboard = () => {
 
     const eventItems = Events.collection.find({}).fetch();
     const listingItems = Positions.collection.find({}).fetch();
+    const companyItem = Companies.collection.findOne({ _id: Meteor.userId() });
 
     return {
       events: eventItems,
       listings: listingItems,
+      company: companyItem,
       ready: rdy,
     };
   }, []);
 
-  const sampleEvents = (events.length > 0) ? events : [
-    {
-      eventName: 'This is a sample event',
-      image: '/images/sample-image-landscape.png',
-      createdAt: 'April 13, 2023',
-    },
-    {
-      name: 'This is another sample event',
-      image: '/images/sample-image-landscape.png',
-      createdAt: 'April 15, 2023',
-    },
-    {
-      name: 'This is a final sample event',
-      image: '/images/sample-image-landscape.png',
-      createdAt: 'April 17, 2023',
-    },
-  ];
+  /** @type { ( array: { tag: string }[]) => { tag: string }[][] } */
+  const groupDocsByTag = (array) => {
+    /** @type { { tag: string }[][] } */
+    const groupArray = [];
+    const sortedByTags = array.sort(({ tag: tag1 }, { tag: tag2 }) => tag1.localeCompare(tag2));
+    let nextGroup;
+    sortedByTags.forEach((doc) => {
+      nextGroup = groupArray[groupArray.length - 1];
+      if (groupArray.length > 0 && doc.tag === nextGroup[0].tag) {
+        nextGroup.push(doc);
+      } else {
+        groupArray.push([doc]);
+      }
+    });
+    return groupArray;
+  };
+
+  /**
+   * Given a job listing, fetch the number of students who saved it.
+   */
+  const getNumSaved = (listing) => {
+    // Do something, probably using a method since pub/sub for this will be expensive
+    const numSaved = 1;
+    return numSaved;
+  };
 
   return (
-    <Container id="company-dashboard" className="py-3">
-      <Row className="justify-content-center">
-        {/* <Col xs={3}>
-          <Sidebar />
-        </Col> */}
-        <Col>
-          <main>
-            <section id="upcoming-events">
-              <h2>Upcoming Events</h2>
-              <div className="events">
-                {sampleEvents.map(({ eventName, image, createdAt }, index) => (
-                  <div key={index} className="event">
-                    <h3 className="event-name">{eventName}</h3>
-                    <img src={image} alt="sample" />
-                    <h4 className="event-date">{createdAt}</h4>
-                  </div>
-                ))}
-              </div>
-            </section>
-            <section id="listings">
-              <h2>Listings</h2>
+    <div id="company-dashboard">
+      <main>
+        <section id="upcoming-events">
+          <h2>Your Upcoming Events</h2>
+          <div className="events">
+            {events.map((event) => (
+              <Event key={event._id} event={event} />
+            ))}
+          </div>
+        </section>
+        <section id="listings">
+          <h2>Your Job Listings</h2>
+          <hr />
+          {groupDocsByTag(listings).map((listingGroup) => (
+            <div key={listingGroup[0].tag} className="listing-group">
+              <h3>{listingGroup[0].tag}</h3>
               <hr />
-              <div className="listing-group">
-                <h3>Software engineering</h3>
-                <hr />
-                <div className="listing">
-                  <a href="/" className="listing-name">
-                    Summer Web Developer Internship
-                  </a>
-                  <Badge>Num. Applicants: 400</Badge>
+              {listings.map((listing) => (
+                <div key={listing._id} className="listing">
+                  <Position position={listing} />
+                  <Badge>Saved by {getNumSaved(listing)} {getNumSaved(listing) === 1 ? 'person' : 'people'}</Badge>
                 </div>
-                <div className="listing">
-                  <a href="/" className="listing-name">
-                    Summer DevOps Internship
-                  </a>
-                  <Badge>Num. Applicants: 300</Badge>
-                </div>
-              </div>
-              <div className="listing-group">
-                <h3>Human Resources</h3>
-                <hr />
-                <div className="listing">
-                  <a href="/" className="listing-name">
-                    Summer HR Internship
-                  </a>
-                  <Badge>Num. Applicants: 100</Badge>
-                </div>
-              </div>
-            </section>
-            <section id="calendar">
-              <h2>A calendar widget</h2>
-            </section>
-          </main>
-        </Col>
-      </Row>
+              ))}
+            </div>
+          ))}
+        </section>
+        <section id="calendar">
+          <FullCalendar
+            plugins={[dayGridPlugin]}
+            initialView="dayGridMonth"
+            height="auto"
+            events={events.map((event) => ({
+              id: event._id,
+              title: event.eventName,
+              start: event.createdAt,
+            }))}
+          />
+        </section>
+        <section id="about">
+          <h2>Company Name</h2>
+          {/* <Company company={company} /> */}
+        </section>
+      </main>
       <HelpButton />
-    </Container>
+    </div>
   );
 };
 
