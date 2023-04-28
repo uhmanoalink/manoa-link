8ffe9c66cc98a8ab642e909bbbfa6a4fa328f89b
Winston Co <winstonco@live.com>
2023-04-27 21:34:56 -1000

    clean up methods

--- app/imports/startup/both/Methods.js
+++ app/imports/startup/both/Methods.js
@@ -1,12 +1,16 @@
 import { Meteor } from 'meteor/meteor';
+import { check } from 'meteor/check';
 import { Roles } from 'meteor/alanning:roles';
 
 Meteor.methods({
-  'initUser'({ userId, role }) {
+  initUser(userId, role) {
+    check(userId, String);
+    check(role, /\b(?=\w)(student|company)\b(?<=\w)/);
     Roles.createRole(role, { unlessExists: true });
     Roles.addUsersToRoles(userId, [role]);
   },
-  'deleteUser'({ userId }) {
+  deleteUser(userId) {
+    check(userId, String);
     Meteor.users.remove({ _id: userId });
   },
 });
