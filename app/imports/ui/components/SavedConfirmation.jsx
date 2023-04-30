afdc333019ec3b87e0a39c17efddc4cce54dc03e
Eddie <jeonhg0@gmail.com>
2023-04-29 21:57:26 -1000

    Update SavedConfirmation.jsx

--- app/imports/ui/components/SavedConfirmation.jsx
+++ app/imports/ui/components/SavedConfirmation.jsx
@@ -1,61 +1,41 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import { Button, Modal } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 import { Mongo } from 'meteor/mongo';
 
-/** @type {React.FC<{ collection: Mongo.Collection<Document, Document>, document: { _id: string } }>} */
-const DeleteConfirmation = ({ collection, document }) => {
-
-  const [showConfirm, setShowConfirm] = useState(false);
-
-  /** @type { React.MouseEventHandler<HTMLButtonElement> } */
-  const handleClose = () => {
-    setShowConfirm(false);
-  };
+/** @type {React.FC<{ collection: Mongo.Collection<Document, Document>, student: { _id: string, savedEvents: string[] }, eventId: string }>} */
+const SavedConfirmation = ({ collection, student, eventId }) => {
+  const isEventSaved = student.savedEvents.includes(eventId);
 
   /** @type { React.MouseEventHandler<HTMLButtonElement> } */
-  const handleDelete = () => {
-    collection.remove({ _id: document._id });
-    handleClose();
-  };
-
-  /** @type { React.MouseEventHandler<HTMLButtonElement> } */
-  const handleShowConfirm = (e) => {
-    if (e.ctrlKey) {
-      handleDelete();
+  const handleToggleSave = () => {
+    if (isEventSaved) {
+      collection.update(
+        { _id: student.userId },
+        { $pull: { savedEvents: eventId } },
+      );
     } else {
-      setShowConfirm(true);
+      collection.update(
+        { _id: student.userId },
+        { $addToSet: { savedEvents: eventId } },
+      );
     }
   };
 
   return (
-    <>
-      <Button onClick={handleShowConfirm} variant="danger" size="sm">Delete</Button>
-      <Modal show={showConfirm} onHide={handleClose} centered>
-        <Modal.Header closeButton>
-          <Modal.Title>Are you sure you want to delete?</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <p>This is a permanent action.</p>
-          <p>Hold <code>ctrl</code>/<code>control</code> when you delete to not show this warning.</p>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Nevermind
-          </Button>
-          <Button onClick={handleDelete} variant="danger">Yes, delete</Button>
-        </Modal.Footer>
-      </Modal>
-    </>
+    <Button onClick={handleToggleSave} variant={isEventSaved ? 'warning' : 'primary'} size="sm">
+      {isEventSaved ? 'Unsave' : 'Save'}
+    </Button>
   );
-
 };
 
-DeleteConfirmation.propTypes = {
+SavedConfirmation.propTypes = {
   collection: PropTypes.instanceOf(Mongo.Collection).isRequired,
-  document: PropTypes.shape({
-    _id: PropTypes.string,
+  student: PropTypes.shape({
+    userId: PropTypes.string,
+    savedEvents: PropTypes.arrayOf(PropTypes.string),
   }).isRequired,
+  eventId: PropTypes.string.isRequired,
 };
 
-export default DeleteConfirmation;
+export default SavedConfirmation;
