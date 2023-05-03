acb6c8fb2e289e117f7e595226e674b3fdbfe58f
Prayag Das <80178328+Prayag-Das@users.noreply.github.com>
2023-05-02 21:03:07 -1000

    148 add save listing functionality for student (#157)
    * Added save job button, but still need to link to adding Jobs to database
    
    * created saveJob, updated Listing.jsx, JobListings.jsx
    
    * removed hard coded data on StudentDashboard's saved jobs tab, which can now display saved jobs from the database
    
    * fixed save/unsave jobs
    
    * Updated Save Job button to a Protected route for Student role
    
    * FIXED

--- app/imports/ui/components/Listing.jsx
+++ app/imports/ui/components/Listing.jsx
@@ -1,37 +1,62 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Card, Col } from 'react-bootstrap';
+import { Badge, Button, Card, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { useTracker } from 'meteor/react-meteor-data';
+import { Meteor } from 'meteor/meteor';
+import { Roles } from 'meteor/alanning:roles';
 import { Images } from '../../api/image/Image';
+import { Listings } from '../../api/listing/Listing';
+import SavedJob from './SavedJob';
+import { Students } from '../../api/student/Student';
+import ProtectedRender from './ProtectedRender';
+import SavedConfirmation from './SavedConfirmation';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 
 const defaultImage = 'images/sample-image-landscape.png';
 
-function addHttpAndWww(url) {
-  console.log(url);
-  let newUrl = url;
-  if (!url.startsWith('http') && !url.startsWith('https')) {
-    newUrl = `https://${newUrl}`;
+const Listing = ({ listing }) => {
+  const { ready, student } = useTracker(() => {
+    const sub = Meteor.subscribe(Students.studentPublicationName);
+    const studentsItems = Students.collection.findOne({ userId: Meteor.userId() });
+    return {
+      ready: sub.ready(),
+      student: studentsItems,
+    };
+  }, []);
+
+  function addHttpAndWww(url) {
+    console.log(url);
+    let newUrl = url;
+    if (!url.startsWith('http') && !url.startsWith('https')) {
+      newUrl = `https://${newUrl}`;
+    }
+    const hostname = new URL(newUrl).hostname;
+    if (!hostname.startsWith('www.')) {
+      newUrl = `https://www.${hostname}`;
+    }
+    return newUrl;
   }
-  const hostname = new URL(newUrl).hostname;
-  if (!hostname.startsWith('www.')) {
-    newUrl = `https://www.${hostname}`;
-  }
-  return newUrl;
-}
-const Listing = ({ listing }) => (
-  <Col xs={12} md={4}>
-    <Card className="justify-content-center" id="listing-card">
-      <Card.Img id="listing-card-image" variant="top" src={(listing.imageId === 'noId') ? defaultImage : Images.getFileUrlFromId(listing.imageId)} />
-      <Card.Title id="listing-card-title">{listing.title}</Card.Title>
-      <Card.Text id="listing-card-text">Company: {listing.companyId}</Card.Text>
-      <Card.Text id="listing-card-text">{listing.description}</Card.Text>
-      <Card.Link id="listing-card-link" href={addHttpAndWww(listing.website)} target="_blank">
-        <button type="button" className="visit-button">More Info</button>
-      </Card.Link>
-    </Card>
-  </Col>
-);
+
+  return (
+    <Col xs={12} md={4}>
+      <Card className="justify-content-center" id="listing-card">
+        <Card.Img id="listing-card-image" variant="top" src={(listing.imageId === 'noId') ? defaultImage : Images.getFileUrlFromId(listing.imageId)} />
+        <Card.Title id="listing-card-title">{listing.title}</Card.Title>
+        <Card.Text id="listing-card-text">Company: {listing.companyId}</Card.Text>
+        <Card.Text id="listing-card-text">{listing.description}</Card.Text>
+        <div>
+          <Card.Link id="listing-card-link" href={addHttpAndWww(listing.website)} target="_blank">
+            <button type="button" className="visit-button">More Info</button>
+          </Card.Link>
+          <ProtectedRender allowedRoles={['student']}>{(student && ready) ? (<SavedJob jobID={listing._id} student={student} collection={Students.collection} />) : undefined}</ProtectedRender>
+        </div>
+      </Card>
+    </Col>
+  );
+};
+
 // Require a document to be passed to this component.
 Listing.propTypes = {
   listing: PropTypes.shape({
